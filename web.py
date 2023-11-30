9fcfc9323c1edbd96d349fdef4e2d2772aa9426d
AMO-SHADROU <106986563+AMO-SHADROU@users.noreply.github.com>
2023-11-30 11:53:20 +0330

    implement the complete future.

--- web.py
+++ web.py
@@ -13,6 +13,11 @@ def add_todo():
 st.title("My Todo App")
 
 for todo in todos:
-    st.checkbox(todo)
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.remove(todo)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
